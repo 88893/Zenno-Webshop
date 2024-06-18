007660d6f403a71fd9e2b4955c8ed81c013e79a6
Joshua Mohanlal <122598877+88893@users.noreply.github.com>
2024-06-18 17:19:08 +0200

    Delete Zenno-master directory

--- Zenno-master/src/backend/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "./AuthProvider";
-
-const PrivateRoute = ({ element: Component }) => {
-  const { user } = useAuth();
-  return user ? <Component /> : <Navigate to="/log-in" />;
-};
-
-export default PrivateRoute;
